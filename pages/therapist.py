201472094471997c5873a22de86556bb918db6a8
andrea057-cyber <andrea.lynn057@gmail.com>
2025-09-21 21:02:19 -0400

    Update therapist.py

--- pages/therapist.py
+++ pages/therapist.py
@@ -19,7 +19,7 @@ if "therapist_messages" not in st.session_state:
 messages = st.session_state.therapist_messages 
 
 st.title("Cyberbullying Support")
-
+st.file_uploader(label="Upload file", type=None, accept_multiple_files=True, key=None, help=None, on_change=None, args=None, kwargs=None, disabled=False, label_visibility="visible", width="stretch")
 # ✅ Show chat history
 for msg in messages:
     if msg["role"] != "system":
